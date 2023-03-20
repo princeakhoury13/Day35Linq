8cf563d1816b82127aec9c310be6a82e108ebdf6
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-20 08:28:43 +0530

    added: List of product reviews

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ProductReviewManagement
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management Problem ");
+
+            List<ProductReview> productReviews = new List<ProductReview>()
+            {
+                new ProductReview() {ProductId =1, UserId=1, Rating=5, Review= "Good", isLike=true},
+                new ProductReview() {ProductId =2, UserId=1, Rating=4, Review= "Good", isLike=true},
+                new ProductReview() {ProductId =3, UserId=2, Rating=5, Review= "Good", isLike=true},
+                new ProductReview() {ProductId =4, UserId=2, Rating=4, Review= "Good", isLike=true},
+                new ProductReview() {ProductId =5, UserId=3, Rating=2, Review= "Bad", isLike=true},
+                new ProductReview() {ProductId =6, UserId=4, Rating=4, Review= "Good", isLike=true},
+                new ProductReview() {ProductId =1, UserId=3, Rating=5, Review= "Good", isLike=true},
+                new ProductReview() {ProductId =10, UserId=10, Rating=1, Review= "Bad", isLike=false},
+                new ProductReview() {ProductId =11, UserId=10, Rating=1, Review= "Bad", isLike=false},
+                new ProductReview() {ProductId =12, UserId=10, Rating=2, Review= "Bad", isLike=false},
+                new ProductReview() {ProductId =13, UserId=10, Rating=3, Review= "Nice", isLike=true},
+                new ProductReview() {ProductId =14, UserId=10, Rating=4, Review= "Good", isLike=true},
+                new ProductReview() {ProductId =15, UserId=10, Rating=5, Review= "Good", isLike=true},
+                new ProductReview() {ProductId =16, UserId=10, Rating=3, Review= "Nice", isLike=true}
+            };
+
+            //foreach(var list in productReviews)
+            //{
+            //    Console.WriteLine("Product Id:"+list.ProductId + "UserId: "+list.UserId + "Rating:" +list.Rating + "Review:" +list.Review +"Like: "+list.isLike);
+
+            //}
+
+            Management management= new Management();
+            management.TopRecordList(productReviews);
+
+        }
+    }
+}
