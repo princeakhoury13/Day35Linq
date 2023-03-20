020e83d36424ad9d07eb8ceaf923f44884a5d1f3
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-20 08:29:11 +0530

    added: top 3 records

---
+++ Management.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    public class Management
+    {
+        public void TopRecordList(List<ProductReview> review)
+        {
+            var recordData = (from productReviews in review
+                              orderby productReviews.Rating descending
+                              select productReviews).Take(3);
+
+            foreach (var list in recordData)
+            {
+                Console.WriteLine("Product Id: " + list.ProductId + " | UserId: " + list.UserId + " | Rating: " + list.Rating + " | Review: " + list.Review + " | Like: " + list.isLike);
+
+            }
+        }
+    }
+}
